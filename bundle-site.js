724e2cdc278daac78392004bfa1334ca407248aa
Stephen Isienyi <stephen.isienyi@gmail.com>
2024-05-06 15:07:11 -0700

    changing site deployment strategy

---
+++ bundle-site.js
@@ -0,0 +1,30 @@
+var fs = require( 'fs' );
+var path = require( 'path' );
+
+const __dirPathTokens = __dirname.split( path.sep );
+
+try {
+    fs.mkdirSync( 'docs' );
+} catch( e ) {
+    if( e.code !== 'EEXIST' ) {
+        console.error( e );
+        process.exit( 1 );
+    }
+}
+
+fs.cp(
+    path.join( __dirname, 'public' ),
+    path.join( __dirname, 'docs' ),
+    {
+        preserveTimestamps: true,
+        recursive: true
+    },
+    function( err ) {
+        if( err ) {
+            console.error( err );
+            process.exit( 1 );
+        }
+        console.log( 'Site deployment complete.' );
+        process.exit( 0 );
+    }
+);
